282ecc2a6f74bbf5c0976a0b5f0f18b8b89caf77
takotokyo <62727191+tacotokyo@users.noreply.github.com>
2021-04-10 23:20:23 +0900

    coin args can be csv

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"math"
 	"os"
@@ -31,7 +30,7 @@ func main() {
 
 	cc := strings.Split(*coins, ",")
 	for i := 0; i < len(cc); i++ {
-		start(bybit, fmt.Sprintf("%q", cc[i]))
+		start(bybit, strings.TrimSpace(cc[i]))
 	}
 }
 
